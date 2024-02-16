0f5fd5fa391954af0fa365597797f3b32a724dea
Saysamiche <140890694+Saysamiche@users.noreply.github.com>
2024-02-16 19:39:35 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
 "use strict";
 
-const green = function () {
-  document.querySelector("#change").style.backgroundColor = "green";
-};
+// const green = function () {
+//   document.querySelector("#change").style.backgroundColor = "green";
+// };
+// document.querySelector("#change").addEventListener("click", green);
 
-document.querySelector("#change").addEventListener("click", green);
+const changeBackgroundColor = function () {
+  if (document.querySelector("#change").style.backgroundColor === "red") {
+    document.querySelector("#change").style.backgroundColor = "green";
+  } else if (
+    document.querySelector("#change").style.backgroundColor !== "red"
+  ) {
+    document.querySelector("#change").style.backgroundColor = "red";
+  }
+};
+document
+  .querySelector("#change")
+  .addEventListener("click", changeBackgroundColor);
