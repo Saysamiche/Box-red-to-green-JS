a6c161f4ea19fa495cf50f743b7aafbf9a9370ec
Saysamiche <140890694+Saysamiche@users.noreply.github.com>
2024-02-17 22:03:06 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -8,12 +8,14 @@
 const changeBackgroundColor = function () {
   if (document.querySelector("#change").style.backgroundColor === "red") {
     document.querySelector("#change").style.backgroundColor = "green";
-    document.querySelector("#change").textContent = "Please click me for a 'RED' background display";
+    document.querySelector("#change").textContent =
+      "Please click me for a 'RED' background display!";
   } else if (
     document.querySelector("#change").style.backgroundColor !== "red"
   ) {
     document.querySelector("#change").style.backgroundColor = "red";
-    document.querySelector("#change").textContent = "Please click me for a 'GREEN' background display";
+    document.querySelector("#change").textContent =
+      "Please click me for a 'GREEN' background display!";
   }
 };
 document
